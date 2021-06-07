d5e737f310a7cf735e406711f86a79414faa415d
RUCHI667 <84953048+RUCHI667@users.noreply.github.com>
2021-06-07 09:58:21 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>javascript and React.js</h1>
+      <p>
+        JavaScript is a text-based programming language used both on the
+        client-side and server-side that allows you to make web pages
+        interactive.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
